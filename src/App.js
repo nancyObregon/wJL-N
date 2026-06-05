d9e7cb37bdee4c92aa8fe38280d916b68e840261
Nancy Nallely Obregón Espinoza <nancy.obregon@softtek.com>
2026-06-05 16:36:34 -0600

    Se agregan cambios

--- src/App.js
+++ src/App.js
@@ -38,20 +38,20 @@ export default function App() {
     }
   };
 
-  const item = {
-    hidden: {
-      opacity: 0,
-      y: 30
-    },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.5,
-        ease: "easeOut"
-      }
-    }
-  };
+  // const item = {
+  //   hidden: {
+  //     opacity: 0,
+  //     y: 30
+  //   },
+  //   show: {
+  //     opacity: 1,
+  //     y: 0,
+  //     transition: {
+  //       duration: 0.5,
+  //       ease: "easeOut"
+  //     }
+  //   }
+  // };
 
   return (
     <div className="app">
